81c1a5ba76425226f86447e24ac8da54fc1d454a
Alejandro <alejandro.proano@gmail.com>
2023-03-13 09:25:52 -0700

    Add intro to assembly

---
+++ week7/test.s
@@ -0,0 +1,55 @@
+	.file	"test.c"
+	.text
+	.globl	sum
+	.type	sum, @function
+sum:
+.LFB23:
+	.cfi_startproc
+	endbr64
+	leal	(%rdi,%rsi), %eax
+	ret
+	.cfi_endproc
+.LFE23:
+	.size	sum, .-sum
+	.section	.rodata.str1.1,"aMS",@progbits,1
+.LC0:
+	.string	"%d"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB24:
+	.cfi_startproc
+	endbr64
+	subq	$8, %rsp
+	.cfi_def_cfa_offset 16
+	movl	$6, %edx
+	leaq	.LC0(%rip), %rsi
+	movl	$1, %edi
+	movl	$0, %eax
+	call	__printf_chk@PLT
+	movl	$0, %eax
+	addq	$8, %rsp
+	.cfi_def_cfa_offset 8
+	ret
+	.cfi_endproc
+.LFE24:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 11.3.0-1ubuntu1~22.04) 11.3.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	1f - 0f
+	.long	4f - 1f
+	.long	5
+0:
+	.string	"GNU"
+1:
+	.align 8
+	.long	0xc0000002
+	.long	3f - 2f
+2:
+	.long	0x3
+3:
+	.align 8
+4:
